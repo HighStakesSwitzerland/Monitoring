79bd029015bfd29ddadb9151aa4855a2a92e767a
Jackson-DKMG <thomas.lafaille@gmail.com>
2024-06-25 14:43:54 +0200

    update discord alerts

--- NAGIOS/usr_local_nagios/libexec/discord_host_alerts.py
+++ NAGIOS/usr_local_nagios/libexec/discord_host_alerts.py
@@ -6,29 +6,34 @@ from discord import SyncWebhook, Embed
 
 HOOK = "THE DISCORD WEBHOOK LINK HERE: https://......"
 
-def codecolor(alerttype):
-    clr_red = 13632027
-    clr_yel = 16098851
-    clr_grn = 8311585
-
-    if alerttype == 'DOWN':
-        return clr_red
-    elif alerttype == 'UP':
-        return clr_grn
-    else:
-        return clr_yel
+# def codecolor(alerttype):
+#     clr_red = 13632027
+#     clr_yel = 16098851
+#     clr_grn = 8311585
+#
+#     if alerttype == 'DOWN':
+#         return clr_red
+#     elif alerttype == 'UP':
+#         return clr_grn
+#     else:
+#         return clr_yel
 
 
 def main(nagios_input):
     nagios_input.pop(0)
 
+    if nagios_input[2] == 'DOWN':
+        status = '🚨'
+    elif nagios_input[2] == 'UP':
+        status = '✅'
+    else:
+        status = '⚠️'
+
+        # $HOSTNAME$ $HOSTADDRESS$ "$SERVICEDESC$" $SERVICESTATE$ $SERVICEOUTPUT$
+
     webhook = SyncWebhook.from_url(HOOK)
-    # create embed object for webhook
-    embed = Embed(title=nagios_input[0], description=nagios_input[1], color=codecolor(nagios_input[2]))
-    webhook.send("<@CONTACT_ID1>,<@CONTACT_ID2>", embed=embed)  #mind the <> -- the id is in format @45613241654987
-    # add embed object to webhook
-    # webhook.add_embed(embed)
-    #
-    # webhook.execute()
+
+    webhook.send(f"{status}\n**{nagios_input[0]}**\n{nagios_input[1]}\n\n<@CONTACT_ID1")
+
 
 main(sys.argv)
--- NAGIOS/usr_local_nagios/libexec/discord_service_alerts.py
+++ NAGIOS/usr_local_nagios/libexec/discord_service_alerts.py
@@ -2,33 +2,45 @@
 
 import sys
 #from discord_webhook import DiscordWebhook, DiscordEmbed
-from discord import SyncWebhook, Embed
+from discord import SyncWebhook #, Embed
 
-HOOK = "THE DISCORD WEBHOOK LINK HERE: https://......"
+HOOK = "https://discord.com/api/webhooks/953992689152577536/qX5TkFZe2HCwPswnBJVjcVTQ5To6ny-mh5MpIicC5C0RhttyBolh8II_lLojiyV4B-sw"
 
-def codecolor(alerttype):
-    clr_red = 13632027
-    clr_yel = 16098851
-    clr_grn = 8311585
-
-    if alerttype == 'CRITICAL':
-        return clr_red
-    elif alerttype == 'OK':
-        return clr_grn
-    else:
-        return clr_yel
+# def codecolor(alerttype):
+#     clr_red = 13632027
+#     clr_yel = 16098851
+#     clr_grn = 8311585
+#
+#     if alerttype == 'CRITICAL':
+#         return clr_red
+#     elif alerttype == 'OK':
+#         return clr_grn
+#     else:
+#         return clr_yel
 
 
 def main(nagios_input):
     nagios_input.pop(0)
-    line1 = f"{nagios_input[0]} -- {nagios_input[1]}"
+
+    line1 = f"**{nagios_input[0]} -- {nagios_input[1]}**"
     line2 = f"{nagios_input[2]}\n{nagios_input[4]}"
 
+    if nagios_input[3] == 'CRITICAL':
+        status = '🚨'
+    elif nagios_input[3] == 'OK':
+        status = '✅'
+    else:
+        status = '⚠️'
 
     webhook = SyncWebhook.from_url(HOOK)
+
+    webhook.send(f"{status}\n{line1}\n{line2}\n\n<@CONTACT_ID1>, <@CONTACT_ID2>")
+
+
+
     # create embed object for webhook
-    embed = Embed(title=line1, description=line2, color=codecolor(nagios_input[3]))
-    webhook.send("<@CONTACT_ID1>,<@CONTACT_ID2>", embed=embed)  # mind the <> -- the id is in format @45613241654987
+    #embed = Embed(title=line1, description=line2, color=codecolor(nagios_input[3]))
+    # webhook.send( embed=embed)  # mind the <> -- the id is in format @45613241654987
     #
     # # add embed object to webhook
     # webhook.add_embed(embed)
